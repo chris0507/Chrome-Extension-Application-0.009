e7686b6c9dd44dd9b686c8776e277934abfb453c
chris <chris.chan050795@gmail.com>
2024-03-18 02:19:54 +0100

    update

--- react-chrome-ext/src/App.tsx
+++ react-chrome-ext/src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
--- react-chrome-ext/src/pages/Home.tsx
+++ react-chrome-ext/src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
 const Home = () => {
-    console.log("asdfasdfasdf")
   return (
     <div>
       <h1>Home</h1>
+      <NavLink to={"/login"}>
+        <button>login</button>
+      </NavLink>
+      <button>signup</button>
     </div>
   );
 };
